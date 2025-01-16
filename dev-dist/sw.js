26a0cb84b9e9e0d635810a0ab7b5d52b7eaf9a21
abelalvarz <abel.alvarez.dev@outlook.com>
2025-01-16 13:35:55 -0600

    make model for reports page

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.08g5io97oe8"
+    "revision": "0.krh82rng2m"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/UI/App/styled-components/HeadContainer.tsx
+++ src/UI/App/styled-components/HeadContainer.tsx
@@ -2,10 +2,11 @@ import React from 'react'
 
 interface Props {
     children: React.ReactNode;
+    className?:string
 }
-export const HeadContainer = ({ children }: Props) => {
+export const HeadContainer = ({ children, className }: Props) => {
     return (
-        <div className="flex justify-between items-center  h-[5vh] ">
+        <div className={`flex justify-between items-center  h-[5vh] ${className}`}>
             {children}
         </div>
     )
