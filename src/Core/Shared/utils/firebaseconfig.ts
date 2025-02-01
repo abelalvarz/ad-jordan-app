dd7fca4cf54b3cd233b9625befc0d0580630cc55
abelalvarz <abel.alvarez.dev@outlook.com>
2025-02-01 09:49:53 -0600

    added .env file

--- src/Core/Shared/utils/firebaseconfig.ts
+++ src/Core/Shared/utils/firebaseconfig.ts
@@ -1,38 +1,19 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore/lite";
+const env = import.meta.env
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAlLHI88gdRyvKZ43zXfXFRpqJx2V_ZCXs",
-  authDomain: "evangelimo-jordan.firebaseapp.com",
-  projectId: "evangelimo-jordan",
-  storageBucket: "evangelimo-jordan.firebasestorage.app",
-  messagingSenderId: "115625534161",
-  appId: "1:115625534161:web:eb2b6711615f3f8f91dbf0"
+  apiKey: env.VITE_FIREBASE_API_KEY,
+  authDomain: env.VITE_AUTH_DOMAIN,
+  projectId: env.VITE_PROJECT_ID,
+  storageBucket: env.VITE_STORAGE_BUCKET,
+  messagingSenderId: env.VITE_MESSAGE_SENDER_ID,
+  appId: env.VITE_API_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const firebaseApp = getFirestore(app);
-app.automaticDataCollectionEnabled;
-// import { getFirestore } from "firebase/firestore/lite";
-// const env = import.meta.env
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: env.VITE_FIREBASE_API_KEY,
-//   authDomain: env.VITE_AUTH_DOMAIN,
-//   projectId: env.VITE_PROJECT_ID,
-//   storageBucket: env.VITE_STORAGE_BUCKET,
-//   messagingSenderId: env.VITE_MESSAGE_SENDER_ID,
-//   appId: env.VITE_API_ID
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// export const firebaseApp = getFirestore(app);
